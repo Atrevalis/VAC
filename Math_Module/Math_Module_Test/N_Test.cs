54c0a5559d7ef2c9222e47991509d1eed84d5852
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2020-03-18 23:04:55 +0300

    Total_war_with_eror
    Тотальное истребление ошибок

--- Math_Module/Math_Module_Test/N_Test.cs
+++ Math_Module/Math_Module_Test/N_Test.cs
@@ -2,18 +2,56 @@ using NUnit.Framework;
 using Math_Module;
 using System.Collections.Generic;
 
-    namespace Math_Module_Test
+namespace Math_Module_Test
+{
+
+    public class N_Test
     {
-        public class Tests
+        [SetUp]
+        public void Setup()
         {
 
- 
-            [Test]
-            public void PlusPlusTest()
-            {
-            N s = new N(new List<string>()); 
-                Assert.AreEqual(s++,new N(new List<string>()));
-
-            }
         }
+
+        List<string> input = new List<string>();// { "12", "99999999" }
+        List<string> output = new List<string>();// { "12", "99999999" }
+
+        [Test]
+        public void nConstructTest()
+        {
+
+            // Assert.AreEqual(n++, output);
+
+        }
+
+        [Test]
+        public void plusPlusTestOne()
+        {
+            input.Add("12");
+            input.Add("99999999");
+            output.Add("13");
+            output.Add("00000000");
+            N n = new N(input);
+            N t = new N(output);
+            Assert.AreEqual(n++, t);
+
+        }
+
+        [Test]
+        public void plusPlusTestTwo()
+        {
+            input.Add("12");
+            input.Add("00000000");
+            input.Add("00000000");
+            output.Add("12");
+            output.Add("00000000");
+            output.Add("00000001");
+            N n = new N(input);
+            N t = new N(output);
+            Assert.AreEqual(n++, t);
+
+        }
+
     }
+
+}
