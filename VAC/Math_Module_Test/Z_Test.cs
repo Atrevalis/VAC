947983e1b13b9558f38ea9ac54a113b865cc3dd0
atrevalis <atrevalis@mail.ru>
2020-03-24 01:07:14 +0300

    add Z_Test

---
+++ VAC/Math_Module_Test/Z_Test.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using Math_Module;
+using System.Collections.Generic;
+using System;
+namespace Math_Module_Test
+{
+    public class Z_Test
+    {
+        [Test]
+        public void plusTest1()
+        {
+            List<string> input = new List<string>();
+            List<string> output = new List<string>();
+            input.Add("12");
+            input.Add("10000000");
+            output.Add("12");
+            output.Add("10000012");
+            output.Add("10000000");
+            N x = new N(input);
+            N i = new N(input);
+            N t = new N(output);
+            Z X = new Z(x);
+            input.Add("00000000");
+            Z I = new Z(i);
+            Z T = new Z(t);
+            Assert.AreEqual(T, X + I);
+
+        }
+        /*
+        [Test]
+        public void plusTest2()
+        {
+            List<string> input = new List<string>();
+            List<string> output = new List<string>();
+            input.Add("50000000");
+            output.Add("1");
+            output.Add("00000000");
+            N n = new N(input);
+            N i = new N(input);
+            N t = new N(output);
+            Assert.AreEqual(t, n + i);
+
+
+        }
+        [Test]
+        public void plusTestEx()
+        {
+            List<string> input = new List<string>();
+            List<string> output = new List<string>();
+            input.Add("49999999");
+            input.Add("50000000");
+            N n = new N(input);
+            N i = new N(input);
+            Assert.AreNotEqual(null, n + i);
+
+        }
+        */
+    }
+}
