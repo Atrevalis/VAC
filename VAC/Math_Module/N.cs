c463fefa7cdf6e3456697f1d0457af52134e84e7
geniuspopeye <62173577+geniuspopeye@users.noreply.github.com>
2020-03-26 22:24:41 +0300

    Update N.cs

--- VAC/Math_Module/N.cs
+++ VAC/Math_Module/N.cs
@@ -275,7 +275,13 @@ namespace Math_Module
 
         private static N SUB_NDN_N(N first, N second, byte k) // Александр Баталин 9370
         {
-            return null;
+            N sec = MUL_ND_N(second, k);
+            if (COM_NN_D(first, sec) != 1)
+            {
+                return first - sec;
+            }
+            else
+                return null;
         }
 
         private static N DIV_NN_Dk(N first, N second) // Шлемин Роман 9370
