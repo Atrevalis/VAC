a7d12d297a354daa02811aef4c00752fdb9f76be
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2020-04-03 16:38:27 +0300

    q

---
+++ VAC/MVS_Controller/Result.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVS_Controller
+{
+    public partial class Result : Noda
+    {
+        public Result(Form parent, Panel panel) : base(parent, panel)
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VAC/MVS_Controller/if_operator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MVS_Controller
+{
+    public partial class if_operator : Noda
+    {
+        Button down_contacts = new Button();
+        static Image butt_image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Resources\\Image\\Start.png");
+        public if_operator(Form parent, Panel panel) : base(parent, panel)
+        {
+            InitializeComponent();
+            InitializeComponent();
+            SizeChanged += new EventHandler(SizeChange);
+            Controls.Add(down_contacts);
+            BackColor = Color.FromArgb(50, 50, 50);
+            down_contacts.FlatStyle = FlatStyle.Popup;
+            down_contacts.BackgroundImageLayout = ImageLayout.Stretch;
+            down_contacts.Location = new Point((int)(Width - (Width * 0.16)), 0);
+            down_contacts.Size = new Size((int)(Width * 0.16), Height);
+            down_contacts.BackgroundImage = butt_image;
+            down_contacts.BackColor = Color.Gray;
+            down_contacts.Click += new EventHandler(Connect_start);
+            down_contacts.Show();
+            label.MouseDown += new MouseEventHandler(Label_click);
+            label.MouseUp += new MouseEventHandler(Label_up);
+        }
+
+        private static void SizeChange(object sender, EventArgs e)
+        {
+            (sender as if_operator).down_contacts.Location = new Point((int)((sender as Working_data).Width - ((sender as Working_data).Width * 0.16)), 0);
+            (sender as if_operator).down_contacts.Size = new Size((int)((sender as Working_data).Width * 0.16), (sender as Working_data).Height);
+        }
+
+        private static void Connect_start(object sender, EventArgs e)
+        {
+            if (conect_nod != (sender as Button).Parent as Noda)
+            {
+                if (conect_nod == null)
+                {
+                    (sender as Button).BackgroundImage = null;
+                    conect_nod = (sender as Button).Parent as Noda;
+                }
+            }
+            else
+            {
+                (sender as Button).BackgroundImage = butt_image;
+                conect_nod = null;
+            }
+        }
+
+        private static void Label_click(object sender, MouseEventArgs e)
+        {
+            Noda_click((sender as Label).Parent, e);
+        }
+
+        private static void Label_up(object sender, MouseEventArgs e)
+        {
+            Noda_up((sender as Label).Parent, e);
+        }
+    }
+}
