31aaa0d5b22923ab93797be99b91c954d1a77610
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2020-04-04 11:23:29 +0300

    P_test

---
+++ VAC/Math_Module_Test/P_Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Math_Module;
+
+namespace Math_Module_Test
+{
+    [TestClass]
+    class P_Test
+    {
+
+    }
+}
