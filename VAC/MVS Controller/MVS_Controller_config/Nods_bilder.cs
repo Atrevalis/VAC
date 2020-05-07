4e5d3ef28b5186dd7d1a094ce0b7a7494fe508cc
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2020-05-07 20:20:24 +0300

    Продолжаем автономизировать модули

---
+++ VAC/MVS Controller/MVS_Controller_config/Nods_bilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MVS_Controller
+{
+    public class Nods_bilder
+    {
+
+        public static Noda Create(string name, Form parent, Panel panel)
+        {
+            return null;
+        }
+
+    }
+}
