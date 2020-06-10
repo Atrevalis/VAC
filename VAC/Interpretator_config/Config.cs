037419ae72bff76446511bf8dd90274626d43f2f
igorek240101 <48420254+igorek240101@users.noreply.github.com>
2020-06-10 21:51:00 +0300

    Config

---
+++ VAC/Interpretator_config/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpretator_config
+{
+    public class Class1
+    {
+    }
+}
