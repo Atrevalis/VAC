a117e3e083f737c7a314d12cdeb1ccf5d4185efc
atrevalis <atrevalis@mail.ru>
2020-06-27 18:44:14 +0300

    Парочка тестов для M

--- VAC/Math_Module_Test/M_Test.cs
+++ VAC/Math_Module_Test/M_Test.cs
@@ -92,6 +92,75 @@ namespace Math_Module_Test
             M output = M.Create(num);
             Assert.AreEqual(output, input - input2);
         }
+        [TestMethod]
+        public void binMultTest1()
+
+        {
+            string num1 = "0_0_0|0_0_0|0_0_0";
+            string num2 = "1_2_3|4_5_6|7_8_9";
+            M input = M.Create(num1);
+            M input2 = M.Create(num2);
+            M output = M.Create(num1);
+            Assert.AreEqual(output, input * input2);
+        }
+        [TestMethod]
+        public void binMultTest2()
+
+        {
+            string num1 = "0_0_0|0_0_0|0_0_0";
+            string num2 = "1_2_3|4_5_6|7_8_9";
+            M input = M.Create(num1);
+            M input2 = M.Create(num2);
+            M output = M.Create(num1);
+            Assert.AreEqual(output, input2 * input);
+        }
+
+        [TestMethod]
+        public void binMultTest3()
+
+        {
+            string num1 = "2_0|0_2|2_0";
+            string num2 = "2_0_2|0_2_0";
+            string num = "4_0_4|0_4_0|4_0_4";
+            M input = M.Create(num1);
+            M input2 = M.Create(num2);
+            M output = M.Create(num1);
+            Assert.AreEqual(output, input1 * input2);
+        }
+        [TestMethod]
+        public void binMultTest4()
+
+        {
+            string num1 = "2_0|0_2|2_0";
+            string num2 = "2_0_2|0_2_0";
+            string num = "8_0|0_4";
+            M input = M.Create(num1);
+            M input2 = M.Create(num2);
+            M output = M.Create(num1);
+            Assert.AreEqual(output, input1 * input2);
+        }
+
+        [TestMethod]
+        public void binDevTest1()
+
+        {
+            string num1 = "2_0|0_2|2_0";
+            string num2 = "0_0_0|0_0_0";
+            string num = "8_0|0_4";
+            M input = M.Create(num1);
+            M input2 = M.Create(num2);
+            Assert.AreEqual(null, input1 / input2);
+        }
+        [TestMethod]
+        public void binDevTest2()
+
+        {
+            string num1 = "0_0_0|0_0_0";
+            string num2 = "1_2_2|2_1_2|2_2_1";//"-6/10_4/10_4/10|4/10_-6/10_4/10|4/10_4/10_-6/10"
+            M input = M.Create(num1);
+            M input2 = M.Create(num2);
+            Assert.AreEqual(input, input1 / input2);
+        }
     }
 
 }
