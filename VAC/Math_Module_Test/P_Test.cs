278efd30c27367118b8bd06e93e54459b32077ff
CrocoDIEl <62173604+CrocoDIEl@users.noreply.github.com>
2020-06-27 20:17:49 +0300

    Тесты деления и умножения в P

--- VAC/Math_Module_Test/P_Test.cs
+++ VAC/Math_Module_Test/P_Test.cs
@@ -31,5 +31,29 @@ namespace Math_Module_Test
             P output1 = P.Create(output);
             Assert.AreEqual(output1, input1.REM(input3));
         }
+
+        [TestMethod]
+        public void DIVTest1()
+        {
+            string input = "-13x^5+(12i+2)x^2-100x^3+12ix+32";
+            string input2 = "x-1";
+            string output = "-13x^4-13x^3-113x^2+(12i-111)x+111";
+            P input1 = P.Create(input);
+            P input3 = P.Create(input2);
+            P output1 = P.Create(output);
+            Assert.AreEqual(output1, input1.DIV(input3));
+        }
+
+        [TestMethod]
+        public void MULTest1()
+        {
+            string input = "-13x^5+(12i-5)x^2";
+            string input2 = "x^3+6i-3";
+            string output = "-13x^8+(12i-44)x^5+(-66i-57)x^2";
+            P input1 = P.Create(input);
+            P input3 = P.Create(input2);
+            P output1 = P.Create(output);
+            Assert.AreEqual(output1, input1.MUL(input3));
+        }
     }
 }
